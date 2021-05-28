320c77ee003cf7d13fffe94b2ca62283ece329cc
ivSaav <ivo_saavedra@outlook.com>
2021-05-28 19:35:56 +0100

    Add: SocketHandler for tcp connections

--- src/main/g24/Peer.java
+++ src/main/g24/Peer.java
@@ -3,13 +3,11 @@ package main.g24;
 import main.g24.chord.INode;
 import main.g24.chord.Node;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.net.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -22,6 +20,8 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -29,14 +29,68 @@ import java.util.concurrent.TimeUnit;
 
 public class Peer extends Node implements ClientPeerProtocol {
 
+    public static final int BLOCK_SIZE = 1024 * 128;
+
     private final int id;
-    private static final int BLOCK_SIZE = 2048;
+    private final SocketHandler selector;
+
+    private final Map<String, String> filenameHashes; // filename --> fileHash
+
 
 
     // PEER
     public Peer(int id, InetAddress addr, int port) {
         super(id, addr, port);
         this.id = id;
+        this.selector = new SocketHandler(this);
+        this.filenameHashes = new HashMap<>();
+    }
+
+    private void init(String service_ap) throws RemoteException {
+        Registry registry = LocateRegistry.getRegistry();
+        try {
+            String[] active = registry.list();
+            ClientPeerProtocol stub = (ClientPeerProtocol) UnicastRemoteObject.exportObject(this,0);
+
+            //Bind the remote object's stub in the registry
+            registry.bind(service_ap, stub); //register peer object with the name in args[0]
+
+            // look for other peers in the ring
+            ClientPeerProtocol root;
+            if (active.length > 0) {
+                root = (ClientPeerProtocol) registry.lookup(active[0]);
+                // join existing ring
+                this.join((INode) root);
+            }
+            else {
+                System.out.println("Creating ring");
+                this.create();
+            }
+
+        } catch (AlreadyBoundException e) {
+            System.out.println("[R] Object already bound! Rebinding...");
+            registry.rebind(service_ap, this);
+        } catch (ConnectException e) {
+            System.out.println("[R] Could not connect to RMI!");
+            e.printStackTrace();
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println("[#] Peer " + this.id + " ready");
+
+        // chord stabilization protocol
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+        scheduler.scheduleAtFixedRate(() -> {
+            try {
+                this.stabilize();
+                this.fix_fingers();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }, 1, 2, TimeUnit.SECONDS);
+
+        ExecutorService tcpService = Executors.newSingleThreadExecutor();
+        tcpService.execute(selector);
     }
 
     // ---------------------------------
@@ -60,15 +114,19 @@ public class Peer extends Node implements ClientPeerProtocol {
 
             }
 
+            String fileHash = SdisUtils.createFileHash(path, id);
+            this.filenameHashes.put(path, fileHash);
+            if (fileHash == null)
+                return "failure";
+
             // init server
-            succ.storeFile(this.id, path, size);
+            succ.storeFile(this.id, this.getStoragePath(fileHash) , size);
 
             SocketChannel socket = SocketChannel.open();
             socket.connect(new InetSocketAddress(succ.get_address(), succ.get_port()));
 
             int n;
             while ((n = fileChannel.read(buffer)) > 0) {
-
                 // flip before writing
                 buffer.flip();
                 // write buffer to channel
@@ -90,54 +148,7 @@ public class Peer extends Node implements ClientPeerProtocol {
 
     @Override
     public void storeFile(int initId, String fileHash, long fileSize) {
-
-        ExecutorService service = Executors.newSingleThreadExecutor();
-
-        service.execute(() -> {
-            try {
-                Path filePath = Paths.get("./out/" + fileHash);
-                Files.createDirectories(filePath.getParent());
-                Files.deleteIfExists(filePath);
-                Files.createFile(filePath);
-
-                FileChannel fileChannel = FileChannel.open(filePath, StandardOpenOption.WRITE, StandardOpenOption.APPEND);
-
-                ByteBuffer buffer = ByteBuffer.allocate(BLOCK_SIZE);
-
-                ServerSocketChannel serverSocket = ServerSocketChannel.open();
-                serverSocket.socket().bind(new InetSocketAddress(this.addr, this.port));
-
-                int total = 0;
-                System.out.println("FILE SIZE " + fileSize);
-                while (true) {
-                    System.out.println("Waiting for client to connect");
-                    // wait for socket connection
-                    SocketChannel client = serverSocket.accept();
-
-                    int n;
-
-                    // read from tcp channel
-                    while ((n = client.read(buffer)) > 0) {
-
-                        total += n;
-                        System.out.println("READ " + n);
-
-                        // flip before writing
-                        buffer.flip();
-                        fileChannel.write(buffer);
-
-                        buffer.clear();
-                    }
-                    client.close();
-                    fileChannel.close();
-                }
-
-            }
-            catch (IOException ioe) {
-                ioe.printStackTrace();
-            }
-        });
-
+        this.selector.prepareReadOperation(fileHash);
     }
 
     @Override
@@ -166,7 +177,7 @@ public class Peer extends Node implements ClientPeerProtocol {
     }
 
     public String getStoragePath(String fileHash) {
-        return getPeerPath() + "storage" + File.separator + fileHash + File.separator;
+        return getPeerPath() + "storage" + File.separator + fileHash;
     }
 
     public String getRestorePath(String fileName) {
@@ -189,48 +200,7 @@ public class Peer extends Node implements ClientPeerProtocol {
         int port = Integer.parseInt(host[1]);
 
         Peer peer = new Peer(id, addr, port);
+        peer.init(service_ap);
 
-        System.out.println("REG");
-        Registry registry = LocateRegistry.getRegistry();
-        try {
-            String[] active = registry.list();
-            ClientPeerProtocol stub = (ClientPeerProtocol) UnicastRemoteObject.exportObject(peer,0);
-
-            //Bind the remote object's stub in the registry
-            registry.bind(service_ap, stub); //register peer object with the name in args[0]
-
-            // look for other peers in the ring
-            ClientPeerProtocol root = peer;
-            if (active.length > 0) {
-                root = (ClientPeerProtocol) registry.lookup(active[0]);
-                // join existing ring
-                peer.join((INode) root);
-            }
-            else {
-                System.out.println("I'm first");
-                peer.create();
-            }
-
-        } catch (AlreadyBoundException e) {
-            System.out.println("[R] Object already bound! Rebinding...");
-            registry.rebind(service_ap, peer);
-        } catch (ConnectException e) {
-            System.out.println("[R] Could not connect to RMI!");
-            e.printStackTrace();
-        } catch (NotBoundException e) {
-            e.printStackTrace();
-        }
-        System.out.println("[#] Peer " + peer.id + " ready");
-
-        // chord stabilization protocol
-        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
-        scheduler.scheduleAtFixedRate(() -> {
-            try {
-                peer.stabilize();
-                peer.fix_fingers();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }, 1, 2, TimeUnit.SECONDS);
     }
 }
---
+++ src/main/g24/SocketHandler.java
@@ -0,0 +1,128 @@
+package main.g24;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Iterator;
+import java.util.Set;
+
+import static main.g24.Peer.BLOCK_SIZE;
+
+public class SocketHandler implements Runnable {
+
+    private final Peer peer;
+    private Selector selector;
+    private String filepath;
+    private int operation;
+
+
+    public SocketHandler(Peer peer) {
+        this.peer = peer;
+    }
+
+    public void prepareWriteOperation(String filename) {
+        this.filepath = filename;
+        this.operation = SelectionKey.OP_WRITE;
+    }
+
+    public void prepareReadOperation(String filePath) {
+        Path path = Paths.get(filePath);
+        try {
+            // create output file
+            Files.createDirectories(path.getParent());
+            Files.deleteIfExists(path);
+            Files.createFile(path);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        this.filepath = filePath;
+        this.operation = SelectionKey.OP_READ;
+    }
+
+
+    // source: https://www.baeldung.com/java-nio-selector
+    @Override
+    public void run() {
+        try {
+
+            ByteBuffer buffer = ByteBuffer.allocate(BLOCK_SIZE);
+
+            selector = Selector.open();
+            ServerSocketChannel serverSocket = ServerSocketChannel.open();
+            serverSocket.socket().bind(new InetSocketAddress(peer.get_address(), peer.get_port()));
+            serverSocket.configureBlocking(false);
+
+            serverSocket.register(selector, SelectionKey.OP_ACCEPT);
+
+            while (true) {
+
+                selector.select();
+
+                Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iter = selectedKeys.iterator();
+
+                while (iter.hasNext()) {
+
+                    SelectionKey key = iter.next();
+
+                    if (key.isAcceptable()) {
+                        register(selector, serverSocket);
+                    }
+
+                    if (key.isReadable()) {
+                        System.out.println("FILE " + filepath);
+                        readAndSave(buffer, key);
+                    }
+                    else if (key.isWritable()) {
+                        // do stuff
+                    }
+
+                    iter.remove();
+                }
+            }
+
+        }
+        catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    private void register(Selector selector, ServerSocketChannel socketChannel) throws IOException {
+        SocketChannel client = socketChannel.accept();
+        client.configureBlocking(false);
+        client.register(selector, this.operation);
+    }
+
+    private void readAndSave(ByteBuffer buffer, SelectionKey key) throws IOException {
+        SocketChannel client = (SocketChannel) key.channel();
+
+        // open out file
+        Path path = Paths.get(filepath);
+        FileChannel fileChannel = FileChannel.open(path, StandardOpenOption.WRITE, StandardOpenOption.APPEND);
+
+        int n;
+        // read from tcp channel
+        while ((n = client.read(buffer)) > 0) {
+            // flip before writing
+            buffer.flip();
+
+            // write to output file
+            fileChannel.write(buffer);
+
+            buffer.clear();
+        }
+
+        if (n < 0) {
+            System.out.println("Client socket closed.");
+            key.channel().close();
+        }
+    }
+
+}

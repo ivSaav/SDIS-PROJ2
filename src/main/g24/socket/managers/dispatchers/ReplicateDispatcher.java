abf2d4fcd96d637b4c10a84f95bcb87496210106
Tiago Silva <tiagodusilva@gmail.com>
2021-06-02 10:31:46 +0100

    Added replicate message hopping

--- src/main/g24/socket/managers/dispatchers/ReplicateDispatcher.java
+++ src/main/g24/socket/managers/dispatchers/ReplicateDispatcher.java
@@ -19,20 +19,23 @@ import java.rmi.RemoteException;
 
 public class ReplicateDispatcher implements ISocketManagerDispatcher {
     private final Peer peer;
-    private final int key_owner;
+    private final int key;
+    private int last_hop;
     private final String fileHash;
     private final Path path;
 
-    public ReplicateDispatcher(Peer peer, int key_owner, String fileHash, Path path) {
+    protected ReplicateDispatcher(Peer peer, int key, int last_hop, String fileHash, Path path) {
         this.peer = peer;
-        this.key_owner = key_owner;
+        this.key = key;
+        this.last_hop = last_hop;
         this.fileHash = fileHash;
         this.path = path;
     }
 
     private ISocketManager onFileRetrieval() {
+        // Notify to increase the rep_degree
         try {
-            INode node = peer.find_successor(key_owner);
+            INode node = peer.find_successor(key);
             ISocketMessage m = new ReplicatedMessage(peer.get_id(), fileHash);
 
             SocketChannel socket = SocketChannel.open();
@@ -61,18 +64,50 @@ public class ReplicateDispatcher implements ISocketManagerDispatcher {
         return switch (message.get_type()) {
             case FILEHERE -> {
                 FileHereMessage filehere = (FileHereMessage) message;
-                if (filehere.file_at_id == key_owner) {
+                if (filehere.file_at_id == last_hop) {
                     // File will be transfered
                     yield new ReceiveFileSocket(peer, filehere, this::onFileRetrieval);
                 } else {
                     // File elsewhere
-                    System.err.println("NOT YET IMPLEMENTED");
-                    yield null;
+                    yield this.redirectGetFile(filehere);
                 }
             }
 
             default -> null;
         };
+    }
+
+    private ISocketManager redirectGetFile(FileHereMessage fileHere) {
+        try {
+            GetFileMessage message = GetFileMessage.from(peer, fileHash);
+            if (message == null)
+                return null;
+
+            INode file_owner = peer.find_successor(fileHere.file_at_id);
+            this.last_hop = file_owner.get_id();
+
+            SocketChannel socket = SocketChannel.open();
+            socket.configureBlocking(false);
+            socket.connect(file_owner.get_socket_address());
+
+            ISocketManager replicateManager = new SocketManager(() -> {
+                try {
+                    message.send(socket);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+
+                return new SocketManager(this);
+            });
+
+            peer.getSelector().register(socket, replicateManager);
+            return null;
+        }
+        catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        return null;
     }
 
     public static ISocketManager initiateReplicate(Peer peer, String fileHash) {
@@ -88,7 +123,7 @@ public class ReplicateDispatcher implements ISocketManagerDispatcher {
             socket.configureBlocking(false);
             socket.connect(key_owner.get_socket_address());
 
-            ISocketManager restoreManager = new SocketManager(() -> {
+            ISocketManager replicateManager = new SocketManager(() -> {
                 try {
                     message.send(socket);
                 } catch (IOException e) {
@@ -98,14 +133,14 @@ public class ReplicateDispatcher implements ISocketManagerDispatcher {
 
                 try {
                     Path filepath = Paths.get(peer.getStoragePath(fileHash));
-                    return new SocketManager(new ReplicateDispatcher(peer, key_owner.get_id(), fileHash, filepath));
+                    return new SocketManager(new ReplicateDispatcher(peer, file_key, key_owner.get_id(), fileHash, filepath));
                 } catch (RemoteException e) {
                     e.printStackTrace();
                     return null;
                 }
             });
 
-            peer.getSelector().register(socket, restoreManager);
+            peer.getSelector().register(socket, replicateManager);
             return null;
         }
         catch (IOException ioe) {
